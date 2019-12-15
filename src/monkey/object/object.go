55fa12226a5556ea922e8ff4ef539d86f5ceaa4b
chicu <chc1129@gmail.com>
2019-12-15 21:03:53 +0900

    3.5.1 整数リテラル(P.125) fix commit

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -30,7 +30,7 @@ type Boolean struct {
 }
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Booelan) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct {}
 
