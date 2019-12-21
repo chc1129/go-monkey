85a1436dc9bc19862f18034bf30305451b1e054b
chicu <chc1129@gmail.com>
2019-12-21 20:16:09 +0900

    4.2.4 文字列結合(P.184) fix commit

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -212,6 +212,10 @@ func TestErrorHandling(t *testing.T) {
       "unknown operator: BOOLEAN + BOOLEAN",
     },
     {
+      `"Hello" - "World"`,
+      "unknown operator: STRING - STRING",
+    },
+    {
       "if (10 > 1) { true + false; }",
       "unknown operator: BOOLEAN + BOOLEAN",
     },
@@ -328,6 +332,20 @@ func TestStringLiteral(t *testing.T) {
   str, ok := evaluated.(*object.String)
   if !ok {
     t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+  }
+
+  if str.Value != "Hello World!" {
+    t.Errorf("String has wrong value. got=%q", str.Value)
+  }
+}
+
+func TestStringConcatenation(t *testing.T) {
+  input := `"Hello" + " " + "World!"`
+
+  evaluated := testEval(input)
+  str, ok := evaluated.(*object.String)
+  if !ok {
+    t.Fatalf("ojbect is not String. got=%T (%+v)", evaluated, evaluated)
   }
 
   if str.Value != "Hello World!" {
