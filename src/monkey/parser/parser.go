845cd67692cbc21126bd1f4b0a939f55a8f0fbb8
chicu <chc1129@gmail.com>
2019-12-24 06:53:26 +0900

    4.4.3 添字演算子式の構文解析(P.199) fix commit

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -17,6 +17,7 @@ const (
   PRODUCT     // *
   PREFIX      // -X または!X
   CALL        // myFunction(X)
+  INDEX       // array[index]
 )
 
 type (
@@ -64,6 +65,7 @@ func New(l *lexer.Lexer) *Parser {
   p.registerInfix(token.LT, p.parseInfixExpression)
   p.registerInfix(token.GT, p.parseInfixExpression)
   p.registerInfix(token.LPAREN, p.parseCallExpression)
+  p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
   // 2つトークンを読み込む。curTokenとpeekTokenの両方がセットされる
   p.nextToken()
@@ -262,6 +264,7 @@ var precedences = map[token.TokenType]int{
   token.SLASH:    PRODUCT,
   token.ASTERISK: PRODUCT,
   token.LPAREN:   CALL,
+  token.LBRACKET: INDEX,
 }
 
 func (p *Parser) peekPrecedence() int {
@@ -475,4 +478,17 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
   }
 
   return list
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+  exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+  p.nextToken()
+  exp.Index = p.parseExpression(LOWEST)
+
+  if !p.expectPeek(token.RBRACKET) {
+    return nil
+  }
+
+  return exp
 }
